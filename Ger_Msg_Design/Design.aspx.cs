3e5f127a8f79912bd983607f369c96788f7085a3
narciso <narcisocsales@gmail.com>
2014-12-26 19:43:50 -0300

    Atualização para troca de imagens

--- Ger_Msg_Design/Design.aspx.cs
+++ Ger_Msg_Design/Design.aspx.cs
@@ -18,50 +18,42 @@ public partial class Design : System.Web.UI.Page
                       "     <div class='usuario' data-user-id='0' chat-id='0'> " + 
                       "             <div class='usuario_img'><image class='imagem' src='../Imagens/narciso.jpg' height='32px' width='32px'/></div> "  +
                       "             <div class='usuario_nome'><span>Narciso</span></div> " +
-                      "             <div class='usuario_ult_msg'>teste resposta do adriano aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
+                      "             <div class='usuario_ult_msg'>teste resposta do narciso aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
                       "             <div class='usuario_data'>17 jan</div> " + 
                       "     </div>" +
                       "   </td> " + 
                       "   <td> "  +
                       "     <div class='usuario' data-user-id='1' chat-id='1'> " +
-                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/adriano.jpg' height='32px' width='32px'/></div> " +
-                      "             <div class='usuario_nome'><span>Adriano</span></div> " +
-                      "             <div class='usuario_ult_msg'>teste resposta do adriano aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
+                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/gato.jpg' height='32px' width='32px'/></div> " +
+                      "             <div class='usuario_nome'><span>Gato</span></div> " +
+                      "             <div class='usuario_ult_msg'>teste resposta do gato aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
                       "             <div class='usuario_data'>18 jan</div> " + 
                       "     </div>"  +
                       "   </td> " +
                       "   <td> " +
                       "     <div class='usuario' data-user-id='2' chat-id='2'> " +
-                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/fabiano.jpg' height='32px' width='32px'/></div> " +
-                      "             <div class='usuario_nome'><span>Fabiano</span></div> " +
-                      "             <div class='usuario_ult_msg'>teste resposta do fabiano aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
+                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/tigres.jpg' height='32px' width='32px'/></div> " +
+                      "             <div class='usuario_nome'><span>Tigres</span></div> " +
+                      "             <div class='usuario_ult_msg'>teste resposta do tigre aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
                       "             <div class='usuario_data'>19 jan</div> " + 
                       "     </div>" +
                       "   </td> " +
                       "   <td> " +
                       "     <div class='usuario' data-user-id='3' chat-id='3'> " +
-                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/marcos.jpg' height='32px' width='32px'/></div> " +
-                      "             <div class='usuario_nome'><span>Marcos</span></div>" +
-                      "             <div class='usuario_ult_msg'>teste resposta do marcos aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
+                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/borboleta.jpg' height='32px' width='32px'/></div> " +
+                      "             <div class='usuario_nome'><span>borboleta</span></div>" +
+                      "             <div class='usuario_ult_msg'>teste resposta da borboleta aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
                       "             <div class='usuario_data'>20 jan</div> " + 
                       "     </div>" +
                       "   </td> " +
                       "   <td> " +
                       "     <div class='usuario' data-user-id='4' chat-id='4'> " +
-                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/anderson.jpg' height='32px' width='32px'/></div> " +
-                      "             <div class='usuario_nome'><span>Anderson</span></div> " +
-                      "             <div class='usuario_ult_msg'>teste resposta do anderson aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
+                      "             <div class='usuario_img'><image class='imagem' src='../Imagens/cachorro.jpg' height='32px' width='32px'/></div> " +
+                      "             <div class='usuario_nome'><span>Cachorro</span></div> " +
+                      "             <div class='usuario_ult_msg'>teste resposta do cachorro aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
                       "             <div class='usuario_data'>21 jan</div> " + 
                       "     </div>" +
                       "   </td> " +
-                      "   <td> " +
-                      "     <div class='usuario' data-user-id='5' chat-id='5'> " +
-                      "             <div class='usuario_img'><image class='imagem' src='' height='32px' width='32px'/></div> " +
-                      "             <div class='usuario_nome'><span>Jardel</span></div>" +
-                      "             <div class='usuario_ult_msg'>teste resposta do jardel aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div> " +
-                      "             <div class='usuario_data'>22 jan</div> " + 
-                      "     </div>" +
-                      "   </td> " + 
                       " </tr> " + 
                       "</table> " +
                       "</div>";
@@ -114,38 +106,31 @@ public partial class Design : System.Web.UI.Page
         string mensagens = "<div class='mensagem_container'> ";
         if (pessoa == 1)
             mensagens += "    <div class='msg_usuario' data-user-id='1'> " +
-               "        <div class='msg_imagem'><image src='../Imagens/adriano.jpg' height='32px' width='32px'/></div> " +
-               "        <div class='msg_nome'>Adriano</div> " +
-               "        <div class='msg_resposta'><span><p>teste resposta do adriano aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<p></span></div> " +
+               "        <div class='msg_imagem'><image src='../Imagens/gato.jpg' height='32px' width='32px'/></div> " +
+               "        <div class='msg_nome'>Gato</div> " +
+               "        <div class='msg_resposta'><span><p>teste resposta do gato aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<p></span></div> " +
                "        <div class='msg_data'>1/2,1:00pm</div> " + 
                "    </div> ";
         else if(pessoa == 2)
             mensagens += "    <div class='msg_usuario' data-user-id='2'> " +
-               "        <div class='msg_imagem'><image src='../Imagens/fabiano.jpg' height='32px' width='32px'/></div> " +
-               "        <div class='msg_nome'>Fabiano</div> " +
-               "        <div class='msg_resposta'><span><p>teste resposta do fabiano aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></span></div> " +
+               "        <div class='msg_imagem'><image src='../Imagens/tigres.jpg' height='32px' width='32px'/></div> " +
+               "        <div class='msg_nome'>Tigres</div> " +
+               "        <div class='msg_resposta'><span><p>teste resposta do tigre aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></span></div> " +
                "        <div class='msg_data'>2/2,2:00pm</div> " +
                "    </div> ";
         else if(pessoa == 3)
             mensagens += "    <div class='msg_usuario' data-user-id='3'> " +
-               "        <div class='msg_imagem'><image src='../Imagens/marcos.jpg' height='32px' width='32px'/></div> " +
-               "        <div class='msg_nome'>Marcos</div> " +
-               "        <div class='msg_resposta'><span><p>teste resposta do marcos aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></span></div> " +
+               "        <div class='msg_imagem'><image src='../Imagens/borboleta.jpg' height='32px' width='32px'/></div> " +
+               "        <div class='msg_nome'>borboleta</div> " +
+               "        <div class='msg_resposta'><span><p>teste resposta do borboleta aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></span></div> " +
                "        <div class='msg_data'>3/2,3:00pm</div> " +
                "    </div> ";
         else if(pessoa == 4)
             mensagens += "    <div class='msg_usuario' data-user-id='4'> " +
-               "        <div class='msg_imagem'><image src='../Imagens/anderson.jpg' height='32px' width='32px'/></div> " +
-               "        <div class='msg_nome'>Anderson</div> " +
-               "        <div class='msg_resposta'><span><p>teste resposta do anderson aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<p></span></div> " +
+               "        <div class='msg_imagem'><image src='../Imagens/cachorro.jpg' height='32px' width='32px'/></div> " +
+               "        <div class='msg_nome'>Cachorro</div> " +
+               "        <div class='msg_resposta'><span><p>teste resposta do cachorro aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<p></span></div> " +
                "        <div class='msg_data'>4/2,4:00pm</div> " +
-               "    </div> ";
-        else if(pessoa == 5)
-            mensagens += "    <div class='msg_usuario' data-user-id='5'> " +
-               "        <div class='msg_imagem'><image src='../Imagens/jardel.jpg' height='32px' width='32px'/></div> " +
-               "        <div class='msg_nome'>Jardel</div> " +
-               "        <div class='msg_resposta'><span><p>teste resposta do jardel aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p></span></div> " +
-               "        <div class='msg_data'>5/2,5:00pm</div> " +
                "    </div> ";
         
         mensagens += "    <div class='msg_usuario' data-user-id='0'> " +
@@ -201,12 +186,11 @@ public partial class Design : System.Web.UI.Page
 
         ArrayList listaDeUsuarios = new ArrayList();
         listaDeUsuarios.Add("Narciso");
-        listaDeUsuarios.Add("Adriano");
-        listaDeUsuarios.Add("Marcos");
-        listaDeUsuarios.Add("Fabiano");
-        listaDeUsuarios.Add("Anderson");
-        listaDeUsuarios.Add("Jardel");
-
+        listaDeUsuarios.Add("Gato");
+        listaDeUsuarios.Add("Tigres");
+        listaDeUsuarios.Add("Borboleta");
+        listaDeUsuarios.Add("Cachorro");
+        
         int count = 0;
 
         for (int i = 0; i < listaDeUsuarios.Count; i++)
@@ -224,38 +208,31 @@ public partial class Design : System.Web.UI.Page
                           "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
                           "     </div>" +
                           "     </li> ";
-                else if (listaDeUsuarios[i].ToString().Equals("Adriano"))
+                else if (listaDeUsuarios[i].ToString().Equals("Gato"))
                     retorno += //"     <li> " +
                           "     <div class='usuario_pesq' data-user-id='1'> " +
-                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/adriano.jpg' height='32px' width='32px'/></div> " +
+                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/gato.jpg' height='32px' width='32px'/></div> " +
                           "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
                           "     </div>" +
                           "     </li> ";
-                else if (listaDeUsuarios[i].ToString().Equals("Fabiano"))
+                else if (listaDeUsuarios[i].ToString().Equals("Tigres"))
                     retorno += //"     <li> " +
                           "     <div class='usuario_pesq' data-user-id='2'> " +
-                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/fabiano.jpg' height='32px' width='32px'/></div> " +
+                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/tigres.jpg' height='32px' width='32px'/></div> " +
                           "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
                           "     </div>" +
                           "     </li> ";
-                else if (listaDeUsuarios[i].ToString().Equals("Marcos"))
+                else if (listaDeUsuarios[i].ToString().Equals("Borboleta"))
                     retorno += //"     <li> " +
                           "     <div class='usuario_pesq' data-user-id='3'> " +
-                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/marcos.jpg' height='32px' width='32px'/></div> " +
+                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/borboleta.jpg' height='32px' width='32px'/></div> " +
                           "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
                           "     </div>" +
                           "     </li> ";
-                else if (listaDeUsuarios[i].ToString().Equals("Anderson"))
+                else if (listaDeUsuarios[i].ToString().Equals("Cachorro"))
                     retorno += //"     <li> " +
                           "     <div class='usuario_pesq' data-user-id='4'> " +
-                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/anderson.jpg' height='32px' width='32px'/></div> " +
-                          "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
-                          "     </div>" +
-                          "     </li> ";
-                else if (listaDeUsuarios[i].ToString().Equals("Jardel"))
-                    retorno += //"     <li> " +
-                          "     <div class='usuario_pesq' data-user-id='5'> " +
-                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/jardel.jpg' height='32px' width='32px'/></div> " +
+                          "        <div class='usuario_pesq_img'><image class='imagem' src='../Imagens/cachorro.jpg' height='32px' width='32px'/></div> " +
                           "        <div class='usuario_pesq_nome'><span>" + listaDeUsuarios[i] + "</span></div> " +
                           "     </div>" +
                           "     </li> ";
